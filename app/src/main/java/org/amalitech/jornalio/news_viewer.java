cbfea0f8c096feb1c5f1dbdabf6625634a8b681c
clemkay <ckoomson75@gmail.com>
2020-02-02 00:23:23 +0000

    Improved UI

---
+++ app/src/main/java/org/amalitech/jornalio/news_viewer.java
@@ -0,0 +1,15 @@
+package org.amalitech.jornalio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class news_viewer extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.news_viewer);
+    }
+}
